407446d7e554f80b7857ab2630f82c2586a79335
Filipe Santos <filipejs.santos21@gmail.com>
2022-10-06 21:56:25 -0300

    resolvendo problema do código anterior

--- mundo 1/desafios/desafio20.py
+++ mundo 1/desafios/desafio20.py
@@ -1,16 +1,10 @@
-from random import choice
+from random import shuffle
 
 aluno1 = str(input('Qual o nome do 1º aluno? '))
 aluno2 = str(input('Qual o nome do 2º aluno? '))
 aluno3 = str(input('Qual o nome do 3º aluno? '))
 aluno4 = str(input('Qual o nome do 4º aluno? '))
 alunos = [aluno1, aluno2, aluno3, aluno4]
+shuffle(alunos)
 
-ordem = []
-ordem.append(choice(alunos))
-ordem.append(choice(alunos))
-ordem.append(choice(alunos))
-ordem.append(choice(alunos))
-
-
-print(f'A ordem dos alunos será {ordem}.')
+print(f'A ordem dos alunos será {alunos}.')
