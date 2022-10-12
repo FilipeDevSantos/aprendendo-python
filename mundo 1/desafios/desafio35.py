fbb11a5797e3e16fdb401bb76831371d646b3975
Filipe Santos <filipejs.santos21@gmail.com>
2022-10-12 15:12:39 -0300

    resolvendo problemas de alguns códigos

--- mundo 1/desafios/desafio35.py
+++ mundo 1/desafios/desafio35.py
@@ -2,11 +2,7 @@ lado1 = int(input('Digite o tamanho do 1º lado: '))
 lado2 = int(input('Digite o tamanho do 2º lado: '))
 lado3 = int(input('Digite o tamanho do 3º lado: '))
 
-if lado3 <= lado1 + lado2:
-    print('podem formar um triângulo!')
-elif lado1 <= lado2 + lado3:
-    print('podem formar um triângulo!')
-elif lado2 <= lado1 + lado3:
+if lado3 > lado1 + lado2 or lado1 > lado2 + lado3 or lado2 > lado1 + lado3:
     print('podem formar um triângulo!')
 else:
     print('Não pode formar um triângulo!')
