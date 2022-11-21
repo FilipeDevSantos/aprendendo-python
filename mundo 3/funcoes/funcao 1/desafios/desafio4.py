4982b64411587252562c5ab2fcaad7ed21ee6b98
Filipe Santos <filipejs.santos21@gmail.com>
2022-11-21 17:42:14 -0300

    concertando códigos

--- mundo 3/funcoes/funcao 1/desafios/desafio4.py
+++ mundo 3/funcoes/funcao 1/desafios/desafio4.py
@@ -1,8 +1,8 @@
 from time import sleep
 
 
-def maior(* num):
-    print('-='*30)
+def maior(*num):
+    print('-=' * 30)
     print('Analisando os valores passados...')
     num_maior = 0
     for c in range(0, len(num)):
@@ -12,6 +12,7 @@ def maior(* num):
             num_maior = num[c]
     print(f'foram informado {len(num)} valores ao todo')
     print(f'O maior número é {num_maior}.')
+
 
 maior(2, 3, 4, 3412, 21, 3)
 maior(2, 5, 8, 23)
