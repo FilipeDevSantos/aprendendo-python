5ea743bc0090e8693627401701b33b13d80379ad
Filipe Santos <filipejs.santos21@gmail.com>
2022-11-21 20:52:35 -0300

    resolução do primeiro desafio sobre modulos e pacotes

---
+++ mundo 3/modulos e pacotes/desafio/moeda.py
@@ -0,0 +1,14 @@
+def dobro(num):
+    return num * 2
+
+
+def metade(num):
+    return num / 2
+
+
+def aumentar(num, quant):
+    return num + (num * quant) / 100
+
+
+def diminuir(num, quant):
+    return num - (num * quant) / 100
