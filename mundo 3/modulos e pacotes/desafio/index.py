d8d9114907c6c9359ea1cefe346f749d85600b9f
Filipe Santos <filipejs.santos21@gmail.com>
2022-11-21 21:10:58 -0300

    resolução do segundo desafio sobre modulos e pacotes

--- mundo 3/modulos e pacotes/desafio/index.py
+++ mundo 3/modulos e pacotes/desafio/index.py
@@ -2,7 +2,7 @@ import moeda
 
 
 preco = int(input('Digite o preço: R$'))
-print(f'A metade de {preco} é {moeda.metade(preco)}.')
-print(f'O dobro de {preco} é {moeda.dobro(preco)}.')
-print(f'Aumentando 10%, temos {moeda.aumentar(preco, 10)}.')
-print(f'Diminuindo 13%, temos {moeda.diminuir(preco, 13)}.')
+print(f'A metade de {moeda.moeda(preco)} é {moeda.moeda(moeda.metade(preco))}.')
+print(f'O dobro de {moeda.moeda(preco)} é {moeda.moeda(moeda.dobro(preco))}.')
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(preco, 10))}.')
+print(f'Diminuindo 13%, temos {moeda.moeda(moeda.diminuir(preco, 13))}.')
--- mundo 3/modulos e pacotes/desafio/moeda.py
+++ mundo 3/modulos e pacotes/desafio/moeda.py
@@ -12,3 +12,9 @@ def aumentar(num, quant):
 
 def diminuir(num, quant):
     return num - (num * quant) / 100
+
+
+def moeda(num):
+    text = "R${preco:.2f}"
+    result = text.format(preco=num)
+    return result.replace('.', ',')
