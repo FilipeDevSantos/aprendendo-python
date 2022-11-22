2dd2a67fe058aa08b0730314842c1df74fc4983a
Filipe Santos <filipejs.santos21@gmail.com>
2022-11-21 21:38:05 -0300

    resolução do quarto desafio sobre modulos e pacotes

--- mundo 3/modulos e pacotes/desafio/index.py
+++ mundo 3/modulos e pacotes/desafio/index.py
@@ -2,7 +2,4 @@ import moeda
 
 
 preco = int(input('Digite o preço: R$'))
-print(f'A metade de {moeda.moeda(preco)} é {moeda.metade(preco, show=True)}.')
-print(f'O dobro de {moeda.moeda(preco)} é {moeda.dobro(preco, show=True)}.')
-print(f'Aumentando 10%, temos {moeda.aumentar(preco, 10, show=True)}.')
-print(f'Diminuindo 13%, temos {moeda.diminuir(preco, 13, show=True)}.')
+moeda.resumo(preco, 80, 35)
--- mundo 3/modulos e pacotes/desafio/moeda.py
+++ mundo 3/modulos e pacotes/desafio/moeda.py
@@ -30,3 +30,14 @@ def moeda(num):
     text = "R${preco:.2f}"
     result = text.format(preco=num)
     return result.replace('.', ',')
+
+
+def resumo(preco, aum, dim):
+    print('-'*30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-'*30)
+    print(f'Preço analisado: \t{moeda(preco)}.')
+    print(f'Dobro do preço: \t{dobro(preco, show=True)}.')
+    print(f'Metade do preço: \t{metade(preco, show=True)}.')
+    print(f'80% de aumento: \t{aumentar(preco, aum, show=True)}.')
+    print(f'35% de redução: \t{diminuir(preco, dim, show=True)}.')
