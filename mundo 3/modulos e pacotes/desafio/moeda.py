866dfef2f811ebd8b6d414b5d235cca547fc3e7c
Filipe Santos <filipejs.santos21@gmail.com>
2022-11-21 21:17:24 -0300

    resolução do terceiro desafio sobre modulos e pacotes

--- mundo 3/modulos e pacotes/desafio/moeda.py
+++ mundo 3/modulos e pacotes/desafio/moeda.py
@@ -1,17 +1,29 @@
-def dobro(num):
-    return num * 2
+def dobro(num, show=False):
+    if show:
+        return moeda(num * 2)
+    else:
+        return num * 2
 
 
-def metade(num):
-    return num / 2
+def metade(num, show=False):
+    if show:
+        return moeda(num / 2)
+    else:
+        return num / 2
 
 
-def aumentar(num, quant):
-    return num + (num * quant) / 100
+def aumentar(num, quant, show=False):
+    if show:
+        return moeda(num + (num * quant) / 100)
+    else:
+        return num + (num * quant) / 100
 
 
-def diminuir(num, quant):
-    return num - (num * quant) / 100
+def diminuir(num, quant, show=False):
+    if show:
+        return moeda(num - (num * quant) / 100)
+    else:
+        return num - (num * quant) / 100
 
 
 def moeda(num):
